457e120be0738c28d39307da47a8dc9492461291
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2024-07-10 13:04:42 +0530

    remove test folder

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const Demo = artifacts.require("demo");
-
-contract("demo", () => {
-  it("should have initial num value of 10", async () => {
-    const instance = await Demo.deployed();
-    const num = await instance.num();
-    assert.equal(num.toNumber(), 10, "Initial num value is not 10");
-  });
-});

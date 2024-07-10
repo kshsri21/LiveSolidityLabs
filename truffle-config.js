abae3364fd76b08944fa97e9eb50cb8d7f5e0a38
kshsri21 <95063103+kshsri21@users.noreply.github.com>
2024-07-10 18:03:05 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,20 +1,26 @@
 module.exports = {
- 
-    networks: {
-      development: {
-        host: "127.0.0.1", // Localhost (default: none)
-        port: 7545, // Standard Ethereum port (default: none)
-        network_id: "*", // Any network (default: none)
-      },
-    },
- 
- 
-    mocha: {
-   
-    },
+	networks: {},
 
+	// Set default mocha options here, use special reporters etc.
+	mocha: {
+		useColors: false,
+		reporter: "mochawesome",
+		reporterOptions: {
+			reportFilename: process.env.UNIT_TEST_OUTPUT_FILE + ".json",
+			json: true,
+			html: false,
+		},
+	},
 
-    compilers: {
-      solc: {
-        version: "0.8.19",
-}
+	// Configure your compilers
+	compilers: {
+		solc: {
+			version: "0.8.19",
+			settings: {
+				optimizer: {
+					enabled: false,
+				},
+			},
+		},
+	},
+};
